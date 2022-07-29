df0fb5eaf167ae2905baab9770b9a79fdb7ca195
ichane31 <roukeyaassouma@gmail.com>
2022-07-29 13:31:07 +0100

    first commit

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -240,7 +240,7 @@ const Categories = () => {
     const actionBodyTemplate = (rowData) => {
         return (
             <React.Fragment>
-                <Button icon="pi pi-pencil" className="p-button-rounded p-button-warning mr-3" onClick={() => editCategory(rowData)} />
+                <Button icon="pi pi-pencil" className="p-button-rounded p-button-info mr-3" onClick={() => editCategory(rowData)} />
                 <Button icon="pi pi-trash" className="p-button-rounded p-button-danger" onClick={() => confirmDeleteCategory(rowData)} />
             </React.Fragment>
         );
